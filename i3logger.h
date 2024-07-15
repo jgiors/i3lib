1c3baeee30bb1e49ddf705330006ab640f149787
jgiors <jgiors@yahoo.com>
2024-07-15 00:44:15 -0700

    first-pass implementation of i3::Logger

--- i3logger.h
+++ i3logger.h
@@ -1,20 +1,29 @@
 #ifndef I3LOGGER_H
 #define I3LOGGER_H
 
+#include <memory>
 #include <vector>
 #include <ostream>
 
-using std::vector;
-using std::ostream;
-
 namespace i3 {
+    using std::shared_ptr;
+    using std::vector;
+    using std::ostream;
+
+    ///Logger stream class for streaming to multiple streams.
     class Logger {
-        vector<ostream> streams;
+        vector<shared_ptr<ostream>> streams;   ///<Streams which are attached.
     public:
+        ///Attach a stream. The stream is not owned by this object, but must not destruct during this object's lifetime.
+        void attachStream(shared_ptr<ostream> pStream) {
+            streams.push_back(pStream);
+        }
+
+        ///Stream a value to all attached streams.
         template<typename T>
         Logger& operator<<(T& x) {
-            foreach(ostream s : streams) {
-                s << x;
+            foreach(auto pS : streams) {
+                *pS << x;
             }
         }
     };
