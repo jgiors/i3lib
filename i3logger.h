2f4dbb00125f8b52d17a8f27bc5be63148100e97
jgiors <jgiors@yahoo.com>
2024-07-15 12:44:59 -0700

    added i3::core namespace

--- i3logger.h
+++ i3logger.h
@@ -6,29 +6,31 @@
 #include <ostream>
 
 namespace i3 {
-    using std::shared_ptr;
-    using std::vector;
-    using std::ostream;
+    namespace core {
+        using std::shared_ptr;
+        using std::vector;
+        using std::ostream;
 
-    ///Logger stream class for streaming to multiple streams.
-    class Logger {
-        vector<shared_ptr<ostream>> streams;   ///<Streams which are attached.
+        ///Logger stream class for streaming to multiple streams.
+        class Logger {
+            vector<shared_ptr<ostream>> streams;   ///<Streams which are attached.
 
-    public:
-        ///Attach a stream.
-        ///@attention Currently, streams can not be detached.
-        void attachStream(shared_ptr<ostream> pStream) {
-            streams.push_back(pStream);
-        }
-
-        ///Stream a value to all attached streams.
-        template<typename T>
-        Logger& operator<<(T& x) {
-            foreach(auto p : streams) {
-                *p << x;
+        public:
+            ///Attach a stream.
+            ///@attention Currently, streams can not be detached.
+            void attachStream(shared_ptr<ostream> pStream) {
+                streams.push_back(pStream);
             }
-        }
-    };
-}
+
+            ///Stream a value to all attached streams.
+            template<typename T>
+            Logger& operator<<(T& x) {
+                foreach(auto p : streams) {
+                    *p << x;
+                }
+            }
+        };
+    } //namespace core
+} //namespace i3
 
 #endif //I3LOGGER_H
