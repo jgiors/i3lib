98867afe209abedf1969a77bab9c42647fee3aad
jgiors <jgiors@yahoo.com>
2024-07-23 00:18:39 -0700

    WIP Prng

--- i3prng.h
+++ i3prng.h
@@ -7,45 +7,59 @@
 
 namespace i3 {
     namespace core {
-        ///32-bit random number generator of type xorshift128 (128-bit state).
+        ///32-bit pseudorandom number generator of type xorshift128 (128-bit state).
         ///Algorithm "xor128" from p. 5 of Marsaglia, "Xorshift RNGs"
         class Prng {
+        public:
             using std::uint32_t;
 
-            uint32_t a, b, c, d;
-        public:
-            using ByteBuffer = std::vector<uint32_t>;
+            ///Internal PRNG state.
+            struct State {
+                uint32_t a, b, c, d;
+            };
 
+        private:
+            State _state;
+
+        public:
             Prng() = delete;
 
-            ///Crete a Prng seeded with the hash of a seed buffer.
-            Prng(ByteBuffer seed);)
+            ///Create a Prng, using the hash of a seed buffer to initialize.
+            Prng(const std::vector<uint8_t> &seed);
+
+            ///Create a Prng with the provided state.
+            Prng(State state) : _state{state} {}
 
             ///Get a random unsigned 32-bit value.       
-            uint32_t rand32();
+            uint32_t random32();
 
             ///Typical rand function. Returns a value between 0 and n-1 inclusive (or 0 when n == 0).
-            uint32_t rand(uint32_t n);
+            uint32_t random(uint32_t n);
 
             ///Rand function which returns value between 0 and n inclusive.
             uint32_t random0toN(uint32_t n);
 
             ///Pick a random integer in the range [i, j].
-            int32_t range(int32_t i, int32_t j);
+            int32_t randomRange(int32_t i, int32_t j);
 
             ///Return a random double precision float between 0 and 1 inclusive.
-            double real();
+            double RandomReal();
 
             ///Splits the random generator and advances the original one step.
-            random split();
+            Prng split();
 
             ///Splits the random generator, but does not advance the state of the original generator.
             ///@important   Do not split the same generator twice without advancing it,
             ///             otherwise the new generators will be identical and correlated.
-            random split_noMutate();
+            Prng split_noMutate() const;
 
+            ///Split the random generator, including the hash of the supplied parameters buffer
+            ///in the splitting process.
+            ///@note Do not use a long parameters buffer. It is copied within this function.
+            Prgn split_parameterized(const std::vector<uint8_t> &parameters) const;
 
-
+            ///Get the internal state.
+            State state() const { return _state; }
         };
     } //namespace core
 } //namespace i3
