c189218d78916cef411528fc361324c1d57c79cf
jgiors <jgiors@yahoo.com>
2024-07-24 19:57:51 -0700

    corrected checks

--- i3prng.cpp
+++ i3prng.cpp
@@ -8,9 +8,9 @@ Prng(const Prng &prng, std::vector<byte> &parameterBuffer) {
     XXH3_state_t hasher;
     static_assert(sizeof(hasher) == sizeof(_state));
 
-    I3CHECK(XXH3_128bits_reset(&hasher) != XXH_OK);
-    I3CHECK(XXH3_128bits_update(&hasher, parameterBuffer.data, parameterBuffer.size()) != XXH_OK);
-    I3CHECK(XXH3_128bits_update(&hasher, _state, sizeof(_state)) != XXH_OK);
+    I3CHECK(XXH3_128bits_reset(&hasher) == XXH_OK);
+    I3CHECK(XXH3_128bits_update(&hasher, parameterBuffer.data, parameterBuffer.size()) == XXH_OK);
+    I3CHECK(XXH3_128bits_update(&hasher, _state, sizeof(_state)) == XXH_OK);
     _state = reinterpret_cast<State>(XXH3_128bits_digest(&hasher));
     rand32();
 }
