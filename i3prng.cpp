3814858dc92cb3c5df3ba19b6ef4241f8ff626a4
jgiors <jgiors@yahoo.com>
2024-07-31 21:24:40 -0700

    added profile info for Prng entropy and to differentiate between machines

--- i3prng.cpp
+++ i3prng.cpp
@@ -23,51 +23,16 @@ i3::Prng::Prng()
     i3log << "    tickCount64 = " << tickCount64 << "\n";
     i3::util::pushData(buffer, &tickCount64, sizeof(tickCount64));
 
-    ///@todo add more seed data for better entropy, like high res timestamp and computer info
+    HW_PROFILE_INFOA profile;
+    I3CHECK_MSG(GetCurrentHwProfileA(&profile) == 0, i3logErr << "error code " << GetLastError());
+    i3log << "     profile: dwDockInfo=" << profile.dwDockInfo << " szHwProfileGuid=" << " szHwProfileGuid=" << profile.szHwProfileGuid << profile.szHwProfileName;
+    i3::util::pushData(buffer, &profile, sizeof(profile));
 
     ///@todo #tentative log buffer contents
 
     *this = Prng(buffer);
     i3log << "    state = (" << _state.a << ", " << _state.b << ", " << _state.c << ", " << _state.d << ")\n"; 
 }
-
-/*
-static core::random seedRandom(void)
-{
-    core::profileTimestamp profilerTime;
-    time_t t;
-    unsigned long long tickCount64;
-
-    slog(LOG_INFO, "Seeding PRNG...");
-
-    ///@todo Library wrapper for ISO timestamp.
-    t = time(NULL);
-    assert(t > 0);
-    char isoTimestamp[32];
-    release_assert(strftime(isoTimestamp, sizeof(isoTimestamp), "%F %T", localtime(&t)));
-        
-    slog(LOG_INFO, "timestamp = %s, epoch = %lld [0x%016llx]", isoTimestamp, t, t);
-
-    profilerTime = core::profile_getMonotonicTimestamp();
-    assert(profilerTime > 0);
-    slog(LOG_INFO, "High resolution timestamp = 0x%016" PRIx64, profilerTime);
-
-    tickCount64 = GetTickCount64();
-    slog(LOG_INFO, "GetTickCount64() = %lld [0x%016llx]", tickCount64, tickCount64);
-    assert(tickCount64 > 0);
-
-    core::random seed((uint32_t)t, (uint32_t)(tickCount64 * profilerTime),
-        (uint32_t)(tickCount64 * t), (uint32_t)profilerTime);
-
-    ///@todo Fix up this random seed code. I really want it to print the values before hashing.
-    slog(LOG_NOTICE, "random seed = { 0x%08x, 0x%08x, 0x%08x, 0x%08x }", seed.a, seed.b, seed.c, seed.d);
-
-    core::random rnd(seed);
-    slog(LOG_INFO, "initial random state = { 0x%08x, 0x%08x, 0x%08x, 0x%08x }", rnd.a, rnd.b, rnd.c, rnd.d);
-
-    return rnd;
-}
-*/
 
 i3::Prng::Prng(const i3::Prng &prng, std::vector<std::byte> &parameterBuffer) {
     XXH3_state_t hasher;
