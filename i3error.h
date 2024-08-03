6052d877c5c8872c975aa2a84ffae54d4dd66a82
jgiors <jgiors@yahoo.com>
2024-08-02 23:21:29 -0700

    updated comments, formatting, and order of includes

--- i3error.h
+++ i3error.h
@@ -6,15 +6,18 @@
 #include <cstdlib>
 #include <cassert>
 
-#define I3ABORT_MSG(STREAMING_LOG_MESSAGE) do { \
-            STREAMING_LOG_MESSAGE; assert(false); std::abort(); \
-        } while(false)
+///Abort application. In debug builds, asserts prior to abort.
+#define I3ABORT()   do { assert(false); std::abort(); } while(false)
 
-///Check macro which asserts a condition in all builds. On failure, i3abort().
-#define I3CHECK(C) if (!(C)) I3ABORT_MSG(i3logErr << "I3CHECK(" << #C << ") failed!\n")
+///Abort application with streaming log message. In debug builds, asserts prior to abort.
+///@example I3ABORT_MSG(i3logErr << "The horror!\n").
+#define I3ABORT_MSG(STREAMING_LOG_MESSAGE)  do { STREAMING_LOG_MESSAGE; I3ABORT(); } while(false)
 
-///Check macro which asserts a condition in all builds.
-///On failure, print streaming log message and i3abort().
+///Check a condition in all builds (aka release assert). On failure, abort application.
+#define I3CHECK(C)  if (!(C)) I3ABORT_MSG(i3logErr << "I3CHECK(" << #C << ") failed!\n")
+
+///Check a condition in all builds (aka release assert).
+///On failure, print streaming log message and abort application.
 ///@example I3CHECK_MSG(x == 0, i3logErr << "Why is x = " << x << "?\n");
 #define I3CHECK_MSG(CONDITION, STREAMING_LOG_MESSAGE) do { \
             if (!CONDITION) { \
--- i3lib.h
+++ i3lib.h
@@ -3,9 +3,10 @@
 ///@file
 
 #include "i3defs.h"
-#include "i3error.h"
 #include "i3logger.h"
+#include "i3error.h"
 #include "i3math.h"
 #include "i3prng.h"
+#include "i3util.h"
 
 #endif //I3LIB_H
